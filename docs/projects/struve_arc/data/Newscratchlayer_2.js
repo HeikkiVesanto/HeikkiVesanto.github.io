c87b8dcd6b77e2446c4ed5296227951a6638b767
heikkivesanto <heikki.vesanto@gmail.com>
2021-04-15 01:18:07 +0100

    added projects

---
+++ docs/projects/struve_arc/data/Newscratchlayer_2.js
@@ -0,0 +1,9 @@
+var json_Newscratchlayer_2 = {
+"type": "FeatureCollection",
+"name": "Newscratchlayer_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Coords": "X: 25.524238 Y: 61.930303" }, "geometry": { "type": "Point", "coordinates": [ 25.524238250241815, 61.930303057352319 ] } },
+{ "type": "Feature", "properties": { "Coords": "X: 25.533690 Y: 61.926839" }, "geometry": { "type": "Point", "coordinates": [ 25.533690276820323, 61.926839918137603 ] } }
+]
+}
